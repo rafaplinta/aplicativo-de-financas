ea6212e62b7d4d44990fd3c03d01f76be7f4b676
Rafaela Plinta <ra.plinta@gmail.com>
2023-05-12 17:37:43 -0300

    Adiciona requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addEmail } from '../redux/actions';
 
 class Login extends React.Component {
   state = {
@@ -16,13 +19,22 @@ class Login extends React.Component {
     const { email } = this.state;
     const minimunLength = 6;
     const passwordLenght = value.length >= minimunLength;
-    // essa validação foi me mostrada na mentoria, pelo professor o Joel. Detalhes aqui: https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+    // essa validação foi me mostrada na mentoria pelo professor o Joel. Detalhes aqui: https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
     const validationRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
     const validateEmail = validationRegex.test(email); // o test() vai me retornar um true or false.
 
     this.setState({
-      isBtnDisable: !(validateEmail && passwordLenght),
+      isBtnDisable: !(validateEmail && passwordLenght), // se ambas forem verdadeiras (e somente se AMBAS forem verdadeiras), ele recebe o estado FALSe (por conta do !). Se não, não.
     });
+  };
+
+  btnClick = () => {
+    // clicar no botão vai chamar enviar o usuário para a rota /carteira e chamar a ação que salva o email no estado global.
+    const { email } = this.state;
+    const { history, dispatch } = this.props;
+
+    dispatch(addEmail(email)); // eu chamo o dispatch como props e passo a action addEmail como parâmetro pra ele. Essa action, por sua vez, recebe o estado local email como parâmetro e a salva no estadogloal
+    history.push('/carteira');
   };
 
   render() {
@@ -30,7 +42,7 @@ class Login extends React.Component {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <div>
           <input
             type="text"
             value={ email }
@@ -47,11 +59,24 @@ class Login extends React.Component {
             placeholder="Digite sua senha"
             onChange={ this.verifyInputs }
           />
-          <button disabled={ isBtnDisable }>Entrar</button>
-        </form>
+          <button
+            disabled={ isBtnDisable }
+            onClick={ this.btnClick }
+          >
+            Entrar
+          </button>
+        </div>
       </div>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+// aqui eu chamo o connect para que os componentes possam "conversar" entre si e acessar o estado global
+export default connect()(Login);
