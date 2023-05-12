77394e7919e1d665fa23a939e3f03b170b143ef5
Rafaela Plinta <ra.plinta@gmail.com>
2023-05-12 16:09:53 -0300

    Configurações iniciais do Redux

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <input
+            type="text"
+            data-testid="email-input"
+            placeholder="Digite seu e-mail"
+          />
+          <input
+            type="text"
+            data-testid="password-input"
+            placeholder="Digite sua senha"
+          />
+          <button>Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,7 @@
-// Coloque aqui suas actions
+export const ADD_EMAIL = 'ADD_EMAIL';
+export const ADD_PASSWORD = 'ADD_PASSWORD';
+
+export const addEmail = (email) => ({
+  type: ADD_EMAIL,
+  payload: email,
+});
