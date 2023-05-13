a170109e74a41e0580b87c5a2df6cf34a772339e
Rafaela Plinta <ra.plinta@gmail.com>
2023-05-13 08:14:45 -0300

    Adiciona requisito 6  completo e uma parte do 3

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,7 +3,44 @@ import React, { Component } from 'react';
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <form>
+        Valor
+        <label htmlFor="valor">
+          <input data-testid="value-input" id="valor" />
+        </label>
+
+        <label htmlFor="descricao">
+          Descrição
+          <input data-testid="description-input" id="descricao" />
+        </label>
+
+        <label>
+          Selecione a moeda:
+          <select data-testid="currency-input">
+            <option>BRL</option>
+          </select>
+        </label>
+
+        <label>
+          Forma de pagamento:
+          <select data-testid="method-input">
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+        </label>
+
+        <label>
+          Categoria:
+          <select data-testid="tag-input">
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
+import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <WalletForm />
+        <Table />
       </div>
     );
   }
