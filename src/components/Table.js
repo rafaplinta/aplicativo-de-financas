b8505c7c1e6fcff1be7f27c4104abb5f6783b9f4
Rafaela Plinta <ra.plinta@gmail.com>
2023-05-17 09:49:21 -0300

    Adiciona requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
-        <th>Descrição</th>
-        <th>Tag</th>
-        <th>Método de pagamento</th>
-        <th>Valor</th>
-        <th>Moeda</th>
-        <th>Câmbio utilizado</th>
-        <th>Valor convertido</th>
-        <th>Moeda de conversão</th>
-        <th>Editar/Excluir</th>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ Number(expense.value).toFixed(2) }</td>
+              <td>{ expense.exchangeRates[expense.currency].name }</td>
+              {/* acesso a expense na chave exchangeRates, na posição currency, e pego o name */}
+              <td>{ Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }</td>
+              <td>
+                { (Number(expense.exchangeRates[expense.currency].ask)
+                  .toFixed(2) * Number(expense.value)).toFixed(2) }
+
+              </td>
+              <td>Real</td>
+              <td>
+                <button>Editar/Excluit</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
